45688f97034ff84a48df7b7fc71abfc09b40a3e5
funsafemath <funsafemath@proton.me>
2026-01-25 14:11:39 +0000

    Add configurable format layer

--- src/level.rs
+++ src/level.rs
@@ -1,24 +1,25 @@
 use pyo3::prelude::*;
 
-#[pyclass]
-pub(super) struct Level(tracing::Level);
+#[pyclass(name = "Level")]
+#[derive(Clone)]
+pub(super) struct PyLevel(tracing::Level);
 
 #[pymethods]
-impl Level {
+impl PyLevel {
     #[classattr]
-    pub(super) const TRACE: Self = Self(tracing::Level::TRACE);
+    const TRACE: Self = Self(tracing::Level::TRACE);
 
     #[classattr]
-    pub(super) const DEBUG: Self = Self(tracing::Level::DEBUG);
+    const DEBUG: Self = Self(tracing::Level::DEBUG);
 
     #[classattr]
-    pub(super) const INFO: Self = Self(tracing::Level::INFO);
+    const INFO: Self = Self(tracing::Level::INFO);
 
     #[classattr]
-    pub(super) const WARN: Self = Self(tracing::Level::WARN);
+    const WARN: Self = Self(tracing::Level::WARN);
 
     #[classattr]
-    pub(super) const ERROR: Self = Self(tracing::Level::ERROR);
+    const ERROR: Self = Self(tracing::Level::ERROR);
 
     fn __repr__(&self) -> String {
         format!("{:?}", self.0)
@@ -29,8 +30,8 @@ impl Level {
     }
 }
 
-impl From<Level> for tracing::Level {
-    fn from(value: Level) -> Self {
+impl From<PyLevel> for tracing::Level {
+    fn from(value: PyLevel) -> Self {
         value.0
     }
 }
