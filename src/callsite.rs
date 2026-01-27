d057b4ac5e307859a61c60c3d351f0989838423b
funsafemath <funsafemath@proton.me>
2026-01-27 22:11:03 +0000

    extract some event-related code into CallsiteAction

--- src/callsite.rs
+++ src/callsite.rs
@@ -8,8 +8,8 @@ use std::{
 };
 
 use pyo3::Python;
-use tracing::Level;
-use tracing_core::{Kind, callsite::DefaultCallsite};
+use tracing::{Level, Value, level_filters};
+use tracing_core::{Callsite, Kind, LevelFilter, callsite::DefaultCallsite};
 
 use crate::{
     callsite::{default::CallsiteIdentifier, empty::EmptyCallsite, kind::CallsiteKind},
@@ -23,6 +23,7 @@ pub(crate) fn get_or_init_callsite(
     kind: Kind,
 ) -> &'static DefaultCallsite {
     // no need to use rwlock/dashmap as we're forced into singlethreaded execution by GIL
+    // turns out pyo3 has free-threaded python support, so it may be better to use an rwlock
     static CALLSITES: LazyLock<Mutex<HashMap<CallsiteIdentifier, &'static DefaultCallsite>>> =
         LazyLock::new(Mutex::default);
 
@@ -42,4 +43,48 @@ pub(crate) fn get_or_init_callsite(
         .entry(identifier.clone())
         // TODO: add fields
         .or_insert_with(|| default::new_callsite(inspector, identifier))
+}
+
+pub(crate) trait CallsiteAction {
+    type ReturnType;
+
+    fn with_fields_and_values(
+        self,
+        f: impl FnOnce(&'static [&'static str], &[Option<&dyn Value>]) -> Option<Self::ReturnType>,
+    ) -> Option<Self::ReturnType>;
+
+    fn do_if_enabled(
+        callsite: &'static impl Callsite,
+        values: &[Option<&dyn Value>],
+    ) -> Self::ReturnType;
+}
+
+pub(crate) fn do_action<A: CallsiteAction>(
+    py: Python,
+    level: Level,
+    kind: Kind,
+    action: A,
+) -> Option<A::ReturnType> {
+    if level <= level_filters::STATIC_MAX_LEVEL && level <= LevelFilter::current() {
+        action.with_fields_and_values(|fields, values| {
+            // todo: maybe remove the fields from the callsite id,
+            // so filtering by callsite can be done before extracting the fields
+            let callsite = get_or_init_callsite(py, level, fields, kind);
+
+            let enabled = {
+                let interest = callsite.interest();
+                !interest.is_never()
+                // oh not, it's not a stable api
+                    && tracing::__macro_support::__is_enabled(callsite.metadata(), interest)
+            };
+
+            if enabled {
+                Some(A::do_if_enabled(callsite, values))
+            } else {
+                None
+            }
+        })
+    } else {
+        None
+    }
 }
